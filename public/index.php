1d9841f083f9c8f95f0b8cf5c2bc6737413f0d4c
Angel Roberto Ibañez Chavez <angelroberto110211@gmail.com>
2024-01-02 15:40:52 -0600

    ca,bios

--- public/index.php
+++ public/index.php
@@ -10,8 +10,9 @@ use proyecto\Response\Failure;
 use proyecto\Response\Success;
 
 Router::headers();
-error_reporting(E_ALL);
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 Router::get("/", function () {
